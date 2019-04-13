7f873d5e5000de167ddfefbf27fabda8b8f222ab
Shantanu <shantanu.067@gmail.com>
2019-04-13 00:30:59 -0600

    added patrol class

---
+++ src/DecisionMaking/Patrol.cpp
@@ -0,0 +1,67 @@
+#pragma once
+#include "Patrol.h"
+#include "Tick.h"
+
+namespace AIForGames
+{
+	namespace DecisionMaking
+	{
+		namespace BehaviorTrees
+		{
+			Patrol::Patrol(uint8_t i_id, uint8_t i_Points): ITask(i_id), m_patrolPointsCount(i_Points)
+			{	}
+			
+			Patrol::~Patrol()
+			{
+
+			}
+
+			void Patrol::AddPatrolPoint(Node* i_point)
+			{
+				m_pPatrolPoints.emplace_back(i_point);
+				m_patrolPointsCount++;
+			}
+
+			uint8_t Patrol::GetCount()
+			{
+				return m_patrolPointsCount;
+			}
+
+			void Patrol::OnEnter(Tick* i_tick)
+			{
+				i_tick->GetBlackBoard()->SetAction("ActiveAction", m_pAction, 1, m_id);
+			}
+
+			void Patrol::OnExit(Tick* i_tick)
+			{
+
+			}
+
+			void Patrol::OnOpen(Tick* i_tick)
+			{
+
+			}
+
+			void Patrol::OnClose(Tick* i_tick)
+			{
+
+			}
+
+			void Patrol::OnExecute(Tick* i_tick)
+			{
+
+			}
+
+			Action* Patrol::GetAction()
+			{
+				return m_pAction;
+			}
+
+			void Patrol::Run(Tick* i_tick)
+			{
+				OnEnter(i_tick);
+				OnExecute(i_tick);
+			}
+		}
+	}
+}
---
+++ src/DecisionMaking/Patrol.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "ITask.h"
+#include "../GameStructs.h"
+namespace AIForGames
+{
+	namespace DecisionMaking
+	{
+		namespace BehaviorTrees
+		{
+			class Patrol : public ITask
+			{
+			public:
+				Patrol(uint8_t i_id, uint8_t i_Points);
+				~Patrol();
+				void AddPatrolPoint(Node* i_point);
+				uint8_t GetCount();
+				virtual void OnEnter(Tick* i_tick) override;
+				virtual void OnExit(Tick* i_tick) override;
+				virtual void OnOpen(Tick* i_tick) override;
+				virtual void OnClose(Tick* i_tick) override;
+				virtual void OnExecute(Tick* i_tick) override;
+				virtual Action* GetAction() override;
+				virtual void Run(Tick* i_tick) override;
+
+			private:
+				std::vector<Node*> m_pPatrolPoints;
+				uint8_t m_patrolPointsCount;
+				Action* m_pAction;
+			};
+		}
+	}
+}
